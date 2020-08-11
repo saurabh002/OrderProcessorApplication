9df7895c51f5a17bd8edcf732dbef8befba74dfa
Saurabh <saurabh2711@gmail.com>
2020-08-12 03:40:07 +0530

    test case added

--- OrderProcessingAPI.Test/OrderPaymentControllerTests.cs
+++ OrderProcessingAPI.Test/OrderPaymentControllerTests.cs
@@ -12,6 +12,68 @@ namespace OrderProcessingAPI.Test
     public class OrderPaymentControllerTests
     {
 
+        OrderPaymentController controller;
+        Mock<OrderPaymentService> mockPaymentsService;
+
+        public OrderPaymentControllerTests()
+        {
+            controller = new OrderPaymentController(new OrderPaymentService(new OrderPaymentProcessorFactory()));
+            mockPaymentsService = new Mock<OrderPaymentService>();
+        }
+        [Fact]
+        public async Task Book_OK()
+        {
+            var response = await controller.ProcessPayment("book");
+
+            var result = response as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal($"{Helper.RoyalityDepartmentHelper} - { Helper.ComissionHelper}", result.Value);
+        }
+
+        [Fact]
+        public async Task Upgrade_OK()
+        {
+            var response = await controller.ProcessPayment("upgrade");
+
+            var result = response as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal($"{Helper.UpgradeHelper} - { Helper.EmailNotificationSender}", result.Value);
+        }
+
+        [Fact]
+        public async Task Video_OK()
+        {
+            var response = await controller.ProcessPayment("video");
+
+            var result = response as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(Helper.VideoHelper, result.Value);
+        }
+
+        [Fact]
+        public async Task Membership_OK()
+        {
+            var response = await controller.ProcessPayment("membership");
+
+            var result = response as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal($"{Helper.MembershipHelper} - {Helper.EmailNotificationSender}", result.Value);
+        }
+
+        [Fact]
+        public async Task PhysicalProduct_OK()
+        {
+            var response = await controller.ProcessPayment("sproduct");
+
+            var result = response as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal($"{Helper.ShippingHelper} - { Helper.ComissionHelper}", result.Value);
+        }
     }
 }
 
